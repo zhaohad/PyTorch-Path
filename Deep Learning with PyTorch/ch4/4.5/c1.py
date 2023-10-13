1d907df2b6fdbcdda5bc3a4a473e1858b6fb9fd9
hanwei <hanwei@xiaomi.com>
2023-10-13 20:41:13 +0800

    Update
    N/A
    
    Signed-off-by: hanwei <hanwei@xiaomi.com>

--- Deep Learning with PyTorch/ch4/4.5/c1.py
+++ Deep Learning with PyTorch/ch4/4.5/c1.py
@@ -23,3 +23,26 @@ for i, letter in enumerate(line.lower().strip()):
 
 print(f"letter_t = {letter_t}")
 
+
+def clean_words(input_str):
+    punctuation = '.,;:\"!?”“_-'
+    word_list = input_str.lower().replace('\n', ' ').split()
+    word_list = [word.strip(punctuation) for word in word_list]
+    return word_list
+
+
+words_in_line = clean_words(line)
+print(f"words_in_line = {words_in_line}")
+
+word_list = sorted(set(clean_words(text)))
+print(f"word_list = {word_list}")
+
+word2index_dict = {word: i for (i, word) in enumerate(word_list)}
+print(f"word2index_dict = {word2index_dict}")
+
+word_t = torch.zeros(len(words_in_line), len(word2index_dict))
+print(f"word_t.shape = {word_t.shape}")
+for i, word in enumerate(words_in_line):
+    word_index = word2index_dict[word]
+    word_t[i][word_index] = 1
+    print('{:2} {:4} {}'.format(i, word_index, word))
